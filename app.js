bc9581f769f77703c17dce02c2b8dd7a1e8b544c
anhtn <anh16102002tran@gmail.com>
2023-11-19 11:40:35 +0700

    code noel 2023

---
+++ app.js
@@ -0,0 +1,15 @@
+var boxgift = document.querySelector('.box-gift')
+var Close = document.querySelector('.fa-xmark')
+var boxContent = document.querySelector('.box-content')
+var content =document.querySelector('.content')
+
+boxgift.onclick = function(){
+    boxgift.classList.toggle('active')
+    // boxContent.classList.add('active')
+}
+content.onclick = function(){
+    boxContent.classList.add('active')
+}
+Close.onclick = function(){
+    boxContent.classList.remove('active')
+}
